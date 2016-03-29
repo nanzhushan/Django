d8bc64a0c7624ff41bcbd2b4c9a3af15b3d7629c
knight <624867243@qq.com>
2016-03-29 22:39:12 +0800

    update

--- Django写的博客网站/V4-用mysql作为db写的博客/myweb/blog/models.py
+++ Django写的博客网站/V4-用mysql作为db写的博客/myweb/blog/models.py
@@ -1,4 +1,6 @@
 #coding:utf8
+#models定义数据库的信息
+
 from django.db import models
 from django.contrib import admin
 
@@ -8,12 +10,18 @@ class BlogsPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
+    # ip = models.CharField(max_length=150)
 
 #将数据库的信息在后台admin显示，
 #创建BlogPostAdmin类，继承admin.ModelAdmin父类，以列表的形式显示BlogPost的标题和时间。
 
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title','timestamp')
+class BlogPostAdmin(admin.ModelAdmin):           #括号里是父类
+    # list_display = ('title','timestamp')   #后台页面显示着两项
+    list_display = ('id','title','timestamp')   #后台页面,按列表的先后顺序写的
+
+#尝试在数据库里添加一栏
 
 
+
+# admin.site.register(BlogsPost,BlogPostAdmin)     ## 添加到后台，不加的话无法显示
 admin.site.register(BlogsPost,BlogPostAdmin)     ## 添加到后台，不加的话无法显示
