af9e995f4935332c1a14fce7dbbe36ee8e1037d0
knight <624867243@qq.com>
2016-04-05 11:02:35 +0800

    views

--- cmdb之运维资产管理系统/cmdb/hostinfo/views.py
+++ cmdb之运维资产管理系统/cmdb/hostinfo/views.py
@@ -6,47 +6,58 @@ from hostinfo.models import Host
 # Create your views here.
 #定义用户的响应请求
 
-def index(req):
-    print req
-    if req.method == 'POST':
-        hostname = req.POST.get('hostname')
-        ip = req.POST.get('ip')
-        osversion = req.POST.get('osversion')
-        memory = req.POST.get('memory')
-        disk = req.POST.get('disk')
-        vendor_id = req.POST.get('vendor_id')
-        model_name = req.POST.get('model_name')
-        cpu_core = req.POST.get('cpu_core')
-        product = req.POST.get('product')
-        Manufacturer = req.POST.get('Manufacturer')
-        sn = req.POST.get('sn')
-        try:
-            host = Host.objects.get(hostname=hostname)
-        except:
-            host = Host()
+def index(request):
+    # return HttpResponse("aaaaaaaaaa55bbb")
+    posts = Host.objects.all()
+    # return HttpResponse(posts)
+    t = loader.get_template("index.html")
+    c = Context({'posts':posts})
+    return HttpResponse(t.render(c))
 
-        host.hostname = hostname
-        host.ip = ip
-        host.osversion = osversion
-        host.memory = memory
-        host.disk = disk
-        host.vendor_id = vendor_id
-        host.model_name = model_name
-        host.cpu_core = cpu_core
-        host.product = product
-        host.Manufacturer = Manufacturer
-        host.sn = sn
-        host.save()
 
-        return HttpResponse('ok')
-    else:
-        return HttpResponse('no data')
 
-#####################################
+
+# def index(req):
+#     print req
+#     if req.method == 'POST':
+#         hostname = req.POST.get('hostname')
+#         ip = req.POST.get('ip')
+#         osversion = req.POST.get('osversion')
+#         memory = req.POST.get('memory')
+#         disk = req.POST.get('disk')
+#         vendor_id = req.POST.get('vendor_id')
+#         model_name = req.POST.get('model_name')
+#         cpu_core = req.POST.get('cpu_core')
+#         product = req.POST.get('product')
+#         Manufacturer = req.POST.get('Manufacturer')
+#         sn = req.POST.get('sn')
+#         try:
+#             host = Host.objects.get(hostname=hostname)
+#         except:
+#             host = Host()
+#
+#         host.hostname = hostname
+#         host.ip = ip
+#         host.osversion = osversion
+#         host.memory = memory
+#         host.disk = disk
+#         host.vendor_id = vendor_id
+#         host.model_name = model_name
+#         host.cpu_core = cpu_core
+#         host.product = product
+#         host.Manufacturer = Manufacturer
+#         host.sn = sn
+#         host.save()
+#
+#         return HttpResponse('ok')
+#     else:
+#         return HttpResponse('no data')
+
+####################################
 
 
 ##之后我自己修改
-#
+
 # def index(req):
 #     # print req
 #     # return HttpResponse("111")
@@ -56,7 +67,3 @@ def index(req):
 #         return HttpResponse("ok")
 #     else:
 #         return HttpResponse("no data")
-
-
-
-
