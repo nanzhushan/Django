453326ea9af1c9123482b2fefad2dab589190a64
knight <624867243@qq.com>
2016-04-21 17:25:22 +0800

    6699

---
+++ Django的注册登录模块设计/V6-使用Session记录登录信息-常用/mysite5/111.py
@@ -0,0 +1,6 @@
+#coding:utf8
+a = "6"
+if a:
+    print "有"
+else:
+    print "无"
---
+++ Django的注册登录模块设计/V6-使用Session记录登录信息-常用/mysite5/online/urls.py
@@ -0,0 +1,13 @@
+#coding:utf8
+from django.conf.urls import patterns, url
+from online import views
+
+
+urlpatterns = patterns('',
+    url(r'^$', views.login, name='login'),              #登录页
+    url(r'^login/$',views.login,name = 'login'),        #登录页
+    url(r'^regist/$',views.regist,name = 'regist'),      #注册页
+    url(r'^index/$',views.index,name = 'index'),            #登录成功页面
+    url(r'^logout/$',views.logout,name = 'logout'),             #注销
+    url(r'^list/$',views.list,name='list'),                             #list页面来验证是不是需要登录才能进行访问
+)
