72eb1f1ade7c1e95f0e83224f384fbd73c0fedb4
knight <624867243@qq.com>
2016-04-23 01:17:38 +0800

    fenye

---
+++ 分页显示代码/V2/omsknight/asset/views.py
@@ -0,0 +1,102 @@
+# coding:utf8
+from django.http import HttpResponseRedirect,HttpResponse
+from django.template import RequestContext,loader,Context
+from django.shortcuts import render_to_response, get_object_or_404,render
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator              ##分页器
+
+#导入数据库类为了方便
+from id_sql import *
+
+#导入表单类
+from asset.form1 import *
+from asset.models import HostList   ##导入所有主机的模型
+from omsknight import settings
+
+#主机编辑函数,显示所有主机
+
+def host_list(request):
+    page = request.GET.get('page', '')  # 获取key对应的值,也就是page后的变量
+    ##try发生异常就执行except语句
+
+    try:
+        page_num = int(page)
+    except:
+        page_num = 0
+    if page_num == 0:
+        pre_page = 0
+        next_page = 1
+    else:
+        pre_page = page_num - 1
+        next_page = page_num + 1
+
+    posts = HostList.objects.all()
+    postn = posts[page_num * 2:(page_num + 1) * 2]     # 2代表一页显示的记录数量
+
+    c = Context({'list':postn,'next_page':next_page,'pre_page':pre_page})
+
+    return render_to_response('host_list1.html',c)
+
+
+##点击编辑展开详细页面
+def detail(request, id):
+    listhost = HostList.objects.all()
+    h_id = get_object_or_404(HostList, pk=id)
+
+    if request.method == 'GET':
+        # listhost = HostList.objects.all()
+        # h_id = get_object_or_404(HostList, pk=id)
+        return render(request, 'detail.html', {'h_ids': h_id})
+
+
+    if request.method == 'POST':
+        ipnew = request.POST['ip']           #获取表单传过来的值
+        hostnamenew = request.POST['hostname']
+        applicationnew = request.POST['application']
+        remarknew = request.POST['remark']
+
+        # 更新数据表,过了出字段然后update
+        b = HostList.objects.filter(ip=h_id.ip).update(ip=ipnew,hostname=hostnamenew,application=applicationnew,remark=remarknew)
+
+        # return HttpResponse(u"已经通过post方式提交更新数据到数据库中....")
+        return HttpResponseRedirect('/list/')
+
+def create_host(request):
+    listhost = HostList.objects.all()
+
+    if request.method == 'GET':
+        return render(request, 'create_host.html')
+
+#创建对象并保存到数据库中
+    if request.method == 'POST':
+        sql = "SELECT COUNT(id) from asset_hostlist"
+        # 获取所有记录列表
+        cursor.execute(sql)
+        results = cursor.fetchall()  ##是个元组
+        ss = str(results)
+        ss2_1 = int(ss[2])+1
+
+
+        ##定义表单内容
+        ipnew = request.POST['ip']
+        hostnamenew = request.POST['hostname']
+        productnew = request.POST['product']
+        applicationnew = request.POST['application']
+        idc_jgnew = request.POST['idc_jg']
+        statusnew = request.POST['status']
+        remarknew = request.POST['remark']
+
+        ##表单插入数据
+        c = HostList.objects.create(id=ss2_1,ip=ipnew,hostname=hostnamenew,product=productnew,application=applicationnew,idc_jg=idc_jgnew,status=statusnew,remark=remarknew)
+        c.save()
+        # return HttpResponse(u"你已经成功的通过post表单方式添加到了数据库")
+        return HttpResponseRedirect('/list/')
+
+
+##定义删除主机按钮
+def delete_host(request,id):
+    # listhost = HostList.objects.all()
+    HostList.objects.filter(id=id).delete()     ##删除指定数据
+    return HttpResponseRedirect('/list/')
+
