58d4a074328c7305fa0d988bf65ddc581f509337
knight <624867243@qq.com>
2016-04-27 03:06:21 +0800

    7777

---
+++ Django的JSON格式提交表单数据/V3--不加key处理JSON数据/V3.1/ok1/fenye/xxx.py
@@ -0,0 +1,4 @@
+f = open("d:\\4455.txt",'w+')
+li = "5555"
+f.write(li)
+f.close()
---
+++ Django的JSON格式提交表单数据/V3--不加key处理JSON数据/V3.2/ok2/fenye/fenye/urls.py
@@ -0,0 +1,12 @@
+#coding:utf8
+
+from django.conf.urls import include, url
+from django.contrib import admin
+import ff.views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^json/$',ff.views.json,name='json'),
+    url(r'^bd/$',ff.views.bd,name='bd'),
+    # url(r'^jn/$',ff.views.jn,name='jn'),
+]
