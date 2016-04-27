58ebe0772087076cd2c2844e48530b280844c5d2
knight <624867243@qq.com>
2016-04-27 10:07:05 +0800

    3366

---
+++ Django的JSON格式提交表单数据/V4--回调函数--待完成/v4.2/fenye/ff/views.py
@@ -0,0 +1,61 @@
+#coding:utf-8
+
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404,render
+from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteger   #导入分页器
+from django.template import RequestContext,loader,Context
+
+from ff.models import BlogsPost
+import simplejson
+import json
+from json import loads, dumps
+
+
+
+
+# Create your views here.
+
+##确实执行了post的方法
+
+def json(request):
+    if request.method == 'POST':
+        d4 = loads((request.body).decode())    #获取json对象
+        d1 = str(d4)          #转换成str格式
+        data = eval(d1)             #转换成字典
+
+        data.update(name='knight', sex='man')      #获取的json数据进行处理
+
+        f = open("d:\\knight.txt", 'w+')              #获取的json数据入库
+        f.write(data['sex'])
+        f.close()
+        a = {'xx':'oo'}
+        # return HttpResponse(u"文档加载完成....")
+        # return render_to_response('1.html',a)
+        return HttpResponse(data['name']+"--and---"+data['sex'])
+        # return HttpResponse(data,content_type="application/json")
+
+
+    return render_to_response('json.html')
+
+
+
+###定义纯粹表单
+
+def bd(request):
+    if request.method == 'POST':
+        name = request.POST.get('name');
+
+        sex = request.POST['sex']
+        return render_to_response('bd.html',{'name':name,'sex':sex})
+        # return HttpResponse(u'你已经post提交了....')
+    return render_to_response('bd.html')
+
+
+
+
+
+
+
+
+
+
