39bda1cbcac3dcb8671547d859f7b51a64362163
knight <624867243@qq.com>
2016-04-28 19:38:38 +0800

    v8--以后业务的正规做法

---
+++ Django的注册登录模块设计/V8-使用Django的auth表并使用session来进行子页面访问限制/mysite5/111.py
@@ -0,0 +1,4 @@
+#coding:utf8
+f = open('d:\\3344.txt', 'w+')
+f.write("fdsafd")
+f.close()
---
+++ Django的注册登录模块设计/V8-使用Django的auth表并使用session来进行子页面访问限制/mysite5/online/models.py
@@ -0,0 +1,11 @@
+#coding:utf8
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.username
